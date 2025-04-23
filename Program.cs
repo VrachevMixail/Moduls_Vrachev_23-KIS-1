2f56f0d73f960d6d8c35e39d69f1cf0b7608ad9a
VrachevMixail <Vrachev07@mail.ru>
2025-04-23 15:11:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._01._2025____Sin___ВраГав
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите x: ");
+            //double sin = Convert.ToDouble(Console.ReadLine());
+            //double rad = sin / 180 * Math.PI;
+            //Console.WriteLine("Синус угла " + sin + " равен " + Math.Sin(rad));
+            //Console.ReadKey();
+            Console.Write("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите n: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
